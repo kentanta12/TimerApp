0f81ab109e4c1441ac42cb90aa0209352b6f5492
KentaNiki <kentanta@113x33x194x194.ap113.ftth.ucom.ne.jp>
2020-07-08 05:46:08 +0900

    setting picker view & button of autolayout in a view vontroller & add SettingViewController

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.swift
+//  MyTimer
+//
+//  Created by KentaNiki on 2020/07/08.
+//  Copyright © 2020 KentaNiki. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: NSObject {
+
+}
